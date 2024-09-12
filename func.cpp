bb3a5d61c244f740398d7fb93200eb7c14eda55d
Рамазан Кантаев <iv322s13@lwp09.ipa.csc.sibsutis.ru>
2024-09-12 11:21:51 +0700

    braza

---
+++ func.cpp
@@ -0,0 +1,30 @@
+#include "func.hpp"
+
+struct rbtree *rbtree_add(struct rbtree *root, int key, char *value)
+{
+//https://publ.lib.ru/ARCHIVES/L/LEVITIN_Ananiy_V/_Levitin_A.V..html
+}
+struct rbtree *rbtree_lookup(struct rbtree *root, int key)
+{
+
+}
+struct rbtree *rbtree_delete(struct rbtree *root, int key)
+{
+
+}
+struct rbtree *rbtree_min(struct rbtree *root)
+{
+
+}
+struct rbtree *rbtree_max(struct rbtree *root)
+{
+
+}
+void rbtree_free(struct rbtree *root)
+{
+
+}
+void rbtree_print_dfs(struct rbtree *root, int level)
+{
+
+}
---
+++ func.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+struct rbtree *rbtree_add(struct rbtree *root, int key, char *value);
+struct rbtree *rbtree_lookup(struct rbtree *root, int key);
+struct rbtree *rbtree_delete(struct rbtree *root, int key);
+struct rbtree *rbtree_min(struct rbtree *root);
+struct rbtree *rbtree_max(struct rbtree *root);
+void rbtree_free(struct rbtree *root);
+void rbtree_print_dfs(struct rbtree *root, int level);
